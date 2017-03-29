f13b12a76ffe54ec9d37adf3de7221671bfd3349
spencerdodd <spencer.evan.dodd@gmail.com>
2017-03-29 18:27:19 -0400

    Testing gitignore

---
+++ spotplay/bot.py
@@ -0,0 +1,11 @@
+import time
+
+oauth_url = "https://oauth.reddit.com"
+reddit_url = "https://www.reddit.com"
+short_reddit_url = "https://redd.it"
+
+
+class SpotPlayBot:
+	def __init__(self):
+		self.time_started = time.time()
+
