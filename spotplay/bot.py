045db06a454ecc297fd9d9fcf37992328e76abd2
spencerdodd <spencer.evan.dodd@gmail.com>
2017-03-30 10:27:07 -0400

    Bot can now search for context calls in the comments and perform functions based on what the comment asks for. So far only 'uptime' is implemented

--- spotplay/bot.py
+++ spotplay/bot.py
@@ -14,16 +14,24 @@ reddit_url = "https://www.reddit.com"
 short_reddit_url = "https://redd.it"
 
 
+# TODO
+# 1. title of playlist should be the title of the submission that linked the playlist
+# 2. youtube 		-------> 		google play music
+# 3. all sources 	-------> 		spotify
+
+
 class SpotPlayBot:
 	def __init__(self):
+		self.start_time = time.time()
+
 		# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 		# reddit
 		# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 		self.reddit = praw.Reddit(client_id=config.reddit_id,
-							 client_secret=config.reddit_secret,
-							 user_agent=config.user_agent,
-							 username=config.reddit_username,
-							 password=config.reddit_password)
+								  client_secret=config.reddit_secret,
+								  user_agent=config.user_agent,
+								  username=config.reddit_username,
+								  password=config.reddit_password)
 
 		if self.reddit.read_only:
 			raise Exception("Authorization failed! (reddit)")
@@ -40,26 +48,36 @@ class SpotPlayBot:
 		# spotify
 		# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 		try:
-			client_credentials_manager = SpotifyClientCredentials(config.spotify_client_id, config.spotify_client_secret)
+			client_credentials_manager = SpotifyClientCredentials(config.spotify_client_id,
+																  config.spotify_client_secret)
 			self.spotify_api = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 		except SpotifyOauthError:
 			raise Exception("Authorization failed! (spotify)")
 
+		print ("Successfully authorized")
+
+		self.subreddit = self.reddit.subreddit(config.subreddit)
+
+		print ("Connected to subreddit {}".format(config.subreddit))
+
 	def get_spotify_posts(self):
-		subreddit = self.reddit.subreddit(config.subreddit)
+		print ("Searching for spotify playlists to re-host")
 		posts_to_scrape = []
 
-		for submission in subreddit.hot(limit=10):
+		for idx, submission in enumerate(self.subreddit.hot(limit=25)):
 			if "spotify" and "playlist" in submission.url:
 				submission.comments.replace_more()
 				comments = submission.comments.list()
 				previously_processed = False
 				for comment in comments:
-					if config.signature in comment.body:
+					if self.previously_processed(comment):
 						previously_processed = True
 
 				if not previously_processed:
 					posts_to_scrape.append(submission)
+
+		if len(posts_to_scrape) == 0:
+			print ("No new posts to re-host")
 
 		return posts_to_scrape
 
@@ -95,7 +113,7 @@ class SpotPlayBot:
 			song_id = self.get_song_from_search(song)
 			if song_id != config.search_failure_string:
 				print ("Adding {}".format(song.get_search_string()))
-				self.google_api.add_songs_to_playlist(new_playlist_id,song_id)
+				self.google_api.add_songs_to_playlist(new_playlist_id, song_id)
 			else:
 				print ("Could not find {} in Google Play Music".format(song.get_search_string()))
 
@@ -123,27 +141,90 @@ class SpotPlayBot:
 	def post_message_in_thread(self, post, share_link):
 		print ("Posting message to thread")
 		post_text = "Here is an automatically-generated Google Play Music playlist of the songs in the posted Spotify" \
-					" playlist\n\n[Playlist]({})\n\nI am a bot, this was performed automatically.\n" \
-					"If there is a problem with this post, please contact /u/aztechk\n\n*{}*\n\n" \
-					"---------------------\n\n" \
-					"^Source ^code: ^[Github]({})".format(share_link, config.signature, config.github_url)
+					" playlist\n\n[Playlist]({})\n\n{}".format(share_link, config.signature)
 		post.reply(post_text)
+
+	# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+	# context methods
+	# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+	def get_uptime(self, comment):
+		print ("Getting uptime and replying to comment")
+		uptime_seconds = time.time() - self.start_time
+		uptime_hours = uptime_seconds / 60
+		uptime_days = uptime_hours / 24
+
+		message = "**Uptime Statistics for spotplay-bot**\n\nspotplay-bot has been up for:\n\n" \
+				  "{} hours\n\n{}".format(uptime_hours, config.signature)
+
+		comment.reply(message)
+
+		print ("Comment posted!")
+
+	# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+	# base methods
+	# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+	def process_spotify_threads(self):
+		for post in self.get_spotify_posts():
+			post_playlist = self.spotify_list_playlist(post.url)
+			share_link = self.google_create_playlist(post_playlist)
+			self.post_message_in_thread(post, share_link)
+
+			print ("Complete!")
+
+	def process_context_calls(self):
+		"""
+		Scrapes comments of threads in given subreddit for mentions that fit the defined functionality. If there is
+		a mention, then process that functionality.
+			- uptime
+			- convert thread
+			- convert song
+		
+		"""
+		print ("Searching for context calls")
+		context_calls = {
+			"{} uptime".format(config.context_clue): self.get_uptime,
+			# "{} convert thread".format(config.context_clue):self.convert_thread,
+			# "{} convert song".format(config.context_clue): self.convert_song
+		}
+		for submission in self.subreddit.hot(limit=25):
+			submission.comments.replace_more()
+			for comment in submission.comments.list():
+				for context_call in context_calls.keys():
+					if context_call in comment.body and not self.previously_processed(comment):
+						context_calls[context_call](comment)
+
+		print ("No more context calls found")
+
+	def previously_processed(self, comment):
+		previously_processed = False
+		for second_level_comment in comment.replies:
+			if config.signature in second_level_comment.body:
+				previously_processed = True
+
+		return previously_processed
+
+	def restart(self):
+		self.__init__()
 
 	def run(self):
 		while True:
-			for post in self.get_spotify_posts():
-				post_playlist = self.spotify_list_playlist(post.url)
-				share_link = self.google_create_playlist(post_playlist)
-				self.post_message_in_thread(post, share_link)
+			try:
+				self.process_spotify_threads()
+				self.process_context_calls()
 
-				print ("Complete!")
+				time.sleep(10)
 
-			time.sleep(120)
+			except Exception as e:
+				print ("Base error encountered, restarting bot\n{}".format(str(e)))
+				self.restart()
 
 
 def main():
 	bot = SpotPlayBot()
 	bot.run()
+
 
 if __name__ == "__main__":
 	main()
